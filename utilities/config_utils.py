f1bfec89b1e73a97e0d7c7119b6b8ce440200d73
DesC0DE <jaden.humphries@gmail.com>
2024-05-25 02:30:42 -0400

    Attempting to fix "what's due issue"

--- utilities/config_utils.py
+++ utilities/config_utils.py
@@ -4,13 +4,13 @@ from werkzeug.security import generate_password_hash
 from datetime import datetime
 
 # Customer vars
-current_date = datetime.now().strftime('"%A, %B %d, %Y"')
+current_date = datetime.now().strftime('"%B %d"')
 user_query: str = ''
 userQueryCache: dict = {}
-CLIENT_INDEX: str = 'summer2024'
-DATA_CSV: str = 'summer2024.csv'#'demo.csv'
+CLIENT_INDEX: str = 'web-application-development'
+DATA_CSV: str = 'webappdev.csv'#'demo.csv'
 CSV_COLUMN_NAME: str = 'School Material'
-DATA_EMBEDDING_CSV: str = 'summer2024_embeddings.csv'
+DATA_EMBEDDING_CSV: str = 'webappdev_embeddings.csv'
 
 # OpenAI API configuration
 openai_api_key: str = os.getenv('OPENAI_API_KEY')
@@ -28,24 +28,11 @@ SYSTEM_INSTRUCTIONS: str = f"""
                             This student has little knowledge of Web Application Development, so keep that in mind.
                             Be sure to give HTML, CSS, JavaScript, and PHP examples when possible in the proper syntax and format.
                             It is important that you know the current date of today which is {current_date}.
-                            Interperet 'this week' as the week that {current_date} is in. 
+                            We are in the the week that {current_date} is in. 
                             Many questions will be based on the most current assignments.
                             Limit reponses to {OUT_TOKEN_LIMIT} tokens. 
                             IMPORTANT: If your response contains HTML tags for examples, treat it like normal text because the browswer will render it.
                             IMPORTANT: Use ASCII examples for code snippets.
-                            "Weekly Schedule: Week 1: Part I: Web Application Basics Date: May 14 - 18 Topic: Syllabus + Course Intro, Understanding How the Web Works, User Interface Design Readings: Syllabus, SAMS Book Chapter 1, DMMT Book: Introduction Assignments: SAMS CH01 Quiz, DMMT Intro Quiz, Prog01-Web Host, VQ's, Syllabus Quiz"
-                            "Weekly Schedule: Week 2: Part: I Web Application Basics Date: May 20 - 25 Topic: Structuring HTML and Using CSS, Understanding the CSS Box Model and Positioning, Don't Make Me Think Readings: SAMS Book Chapter 2, SAMS Book Chapter 3, DMMT Book: Chapter 1 Assignments: SAMS CH02 Quiz, SAMS CH03 Quiz, DMMT Ch01 Quiz, Prog02 - HTML & CSS, Prog03 - CSS Layout, VQ's"
-                            "Weekly Schedule: Week 3: Part: II Getting Started with Dynamic Websites Date: May 27 - June 1 Topic: Introducing Javascript, Introducing PHP, How We Use the Web Readings: SAMS Book Chapter 4, SAMS Book Chapter 5, DMMT Book: Chapter 2 Assignments: SAMS CH04 Quiz, SAMS CH05 Quiz, DMMT Ch02 Quiz, Prog04 - Javascript Events, Prog05 - PHP HelloWorld, VQ's"
-                            "Weekly Schedule: Week 4: Part: II Getting Started with Dynamic Websites Date: June 3 - 8 Topic: Understanding Dynamic Websites and HTML5 Applications, Javascript Fundamentals: Variables, Strings, Arrays, Billboard Design 101 Readings: SAMS Book Chapter 6, SAMS Book Chapter 7, DMMT Book: Chapter 3 Assignments: SAMS CH06 Quiz, SAMS CH07 Quiz, DMMT Ch03 Quiz, Prog06 - Moving Buttons, Prog07 - Javascript Sort, SAMS Exam 1 (chapters 1-5), VQ's"
-                            "Weekly Schedule: Week 5: Part: II Getting Started with Dynamic Websites Date: June 10 - 15 Topic: Javascript Fundamentals: Functions, Objects, Flow Control, Javascript Event Handling, Animal, Vegetable or Mineral Readings: SAMS Book Chapter 8, SAMS Book Chapter 9, DMMT Book: Chapter 4 Assignments: SAMS CH08 Quiz, SAMS CH09 Quiz, DMMT Ch04 Quiz, Prog08 - Card Object, Prog09 - Keypress, VQ's, Program Exam 1"
-                            "Weekly Schedule: Week 6: Part: Summer Break Date: June 17 - 22"
-                            "Weekly Schedule: Week 7: Part: III Taking Your Web Applications to the Next Level Date: June 24 - 29 Topic: The Basics of jQuery, Omit Words, Street Signs and Breadcrumbs, Big Bang Theory Readings: SAMS Book Chapter 10, DMMT Book: Chapter 5, Chapter 6, Chapter 7 Assignments: SAMS CH10 Quiz, DMMT Ch05 Quiz, DMMT Ch06 Quiz, DMMT Ch07 Quiz, Prog10 - Same Menu, Prog11 - jQuery, jQuery Quiz, Ch10 Source Code Quiz"
-                            "Weekly Schedule: Week 8: Part: III Taking Your Web Applications to the Next Level Date: July 1 - 6 Topic: AJAX: Remote Scripting, PHP Fundamentals: Variables, Strings, Arrays, The Farmer and Cowman, Usability Testing Readings: SAMS Book Chapter 11, SAMS Book Chapter 12, DMMT Book: Chapter 8, Chapter 9 Assignments: SAMS CH11 Quiz, SAMS CH12 Quiz, DMMT Ch08 Quiz, DMMT Ch09 Quiz, Ch11 Source Code Quiz, Prog12 - AJAX & PHP, Project 1"
-                            "Weekly Schedule: Week 9: Part: III Taking Your Web Applications to the Next Level Date: July 8 - 13 Topic: PHP Fundamentals: Functions, Objects, Flow Control, Cookies & User Sessions, Web Forms Readings: SAMS Book Chapter 13, SAMS Book Chapter 14, SAMS Book Chapter 15 Assignments: SAMS CH13 Quiz, SAMS CH14 Quiz, SAMS CH15 Quiz, Prog13 - Ajax & Handlebars, Prog14 - PHP File I/O, SAMS Exam 2 (chapters 6-13)"
-                            "Weekly Schedule: Week 10: Part: IV Integrating a Database into Your Application Date: July 15 - 20 Topic: Database Design, Basic SQL Commands Readings: SAMS Book Chapter 16, SAMS Book Chapter 17 Assignments: Project 2, Program Exam 2"
-                            "Weekly Schedule: Week 11: Part: IV Integrating a Database into Your Application Date: July 22 - 27 Topic: MySQL & PHP, Mobile, Usability Readings: SAMS Book Chapter 18, DMMT Book: Chapter 10, Chapter 11 Assignments: Database Quiz, DMMT Ch10 Quiz, Prog15 - Usability"
-                            "Weekly Schedule: Week 12: Part: IV Integrating a Database into Your Application Date: July 29 - Aug 3 Topic: Accessibility, Guide for the Perplexed Readings: DMMT Book: Chapter 12, Chapter 13 Assignments: Project 3"
-                            "Weekly Schedule: Week 13: Part: Final Exam Section Date: Aug 5 Assignments: SAMS Final Exam, DMMT Final Exam"
                         """
 
 # System Parameters
